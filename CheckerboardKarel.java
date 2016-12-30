803726cb384b8e857213f86beaeb3501fe69f9a5
S <s>
2016-12-30 16:31:06 -0500

    {"types":"C","runs":25}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -12,5 +12,51 @@ import stanford.karel.*;
 public class CheckerboardKarel extends SuperKarel {
 
 	// You fill in this part
-
+	public void run()
+	{
+		while(frontIsClear())
+		{
+			fillLine();
+			endOfLineCheck();
+		}
+	}
+	
+	private void fillLine()
+	{
+		while(frontIsClear())
+		{
+			move();
+			if(frontIsClear())
+			{
+				putBeeper();
+				move();
+			}
+		}
+	}
+	
+	public void endOfLineCheck()
+	{
+		if(facingEast())
+		{
+			putBeeper();
+			turnLeft();
+			if(frontIsClear())
+			{
+				move();
+				turnLeft();
+			}
+		}
+		else
+		{
+			putBeeper();
+			turnRight();
+			if(frontIsClear())
+			{
+				move();
+				turnRight();
+			}
+		}
+		
+	}
+	
 }
