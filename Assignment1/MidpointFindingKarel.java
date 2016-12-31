77f954e44f5e70d52debf4821946e1322ed8d93e
PalashMatey <pm2824@columbia.edu>
2016-12-30 21:47:47 -0500

    Last problem

--- Assignment1/MidpointFindingKarel.java
+++ Assignment1/MidpointFindingKarel.java
@@ -15,5 +15,65 @@ import stanford.karel.*;
 public class MidpointFindingKarel extends SuperKarel {
 
 	// You fill in this part
+	public void run()
+	{
+		fillLine();
+		moveBack();
+		getMidPoint();
+	}
+	
+	private void fillLine()
+	{
+		putBeeper();
+		while(frontIsClear())
+		{
+			move();
+			putBeeper();
+		}
+		turnAround();
+	}
+	private void moveBack()
+	{
+		while (frontIsClear()) {
+			move();
+		}
+		turnAround();
+	}
+	
+	private void moveToLastBeeperAhead()
+	{
+		while(frontIsClear() && beepersPresent())
+		{
+			move();
+		}
+		if (noBeepersPresent())
+		{
+			turnAround();
+			move();
+			turnAround();
+		}
+		
+
+	}
+	
+	private void getMidPoint()
+	{
+		while(beepersPresent())
+		{
+			moveToLastBeeperAhead();
+			pickBeeper();
+			turnAround();
+			if(frontIsClear())
+			{
+				move();
+			}
+		}
+		
+		turnAround();
+		if (frontIsClear()) {
+			move();
+		}
+		putBeeper();
+	}
 
 }
